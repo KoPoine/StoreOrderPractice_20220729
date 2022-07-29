263b6a6d83e8d23bec22f74b24d384a279d3ab13
김민철 <kmc901206@gmail.com>
2022-07-29 09:51:10 +0900

    2. ViewPager의 재료로 사용될 Fragment 2개 생성

---
+++ app/src/main/java/com/neppplus/storeorderpractice_20220729/fragments/ProfileFragment.kt
@@ -0,0 +1,27 @@
+package com.neppplus.storeorderpractice_20220729.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.neppplus.storeorderpractice_20220729.R
+
+class ProfileFragment : Fragment() {
+
+//    layout(xml)과 class(kt) 엮어주는 함수
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+//    Fragment class의 동작 코드
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+    }
+
+}
