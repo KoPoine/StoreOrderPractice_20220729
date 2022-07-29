cdc14aa109734ab3833859f613c0f9a2760cbce9
김민철 <kmc901206@gmail.com>
2022-07-29 17:21:40 +0900

    ProfileActivit 닉네임 변경 로직 작성

--- app/src/main/java/com/neppplus/storeorderpractice_20220729/ProfileActivity.kt
+++ app/src/main/java/com/neppplus/storeorderpractice_20220729/ProfileActivity.kt
@@ -1,11 +1,21 @@
 package com.neppplus.storeorderpractice_20220729
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        saveBtn.setOnClickListener {
+            val inputNick = changeNickEdt.text.toString()
+            val resultIntent = Intent()
+            resultIntent.putExtra("changedNick", inputNick)
+            setResult(RESULT_OK, resultIntent)
+            finish()
+        }
     }
 }
--- app/src/main/java/com/neppplus/storeorderpractice_20220729/fragments/ProfileFragment.kt
+++ app/src/main/java/com/neppplus/storeorderpractice_20220729/fragments/ProfileFragment.kt
@@ -1,13 +1,19 @@
 package com.neppplus.storeorderpractice_20220729.fragments
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.neppplus.storeorderpractice_20220729.ProfileActivity
 import com.neppplus.storeorderpractice_20220729.R
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileFragment : Fragment() {
+
+    val REQ_FOR_NICK = 1000
 
 //    layout(xml)과 class(kt) 엮어주는 함수
     override fun onCreateView(
@@ -21,7 +27,20 @@ class ProfileFragment : Fragment() {
 //    Fragment class의 동작 코드
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        changeNickBtn.setOnClickListener {
+            val myIntent = Intent(requireContext(), ProfileActivity::class.java)
+            startActivityForResult(myIntent, REQ_FOR_NICK)
+        }
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == REQ_FOR_NICK) {
+                val changedNick = data?.getStringExtra("changedNick")
+                nickTxt.text = changedNick
+            }
+        }
     }
 
 }
