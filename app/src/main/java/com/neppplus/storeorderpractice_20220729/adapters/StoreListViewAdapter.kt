393f4a71d4e19dacd864b51a387c42e436dbebe2
김민철 <kmc901206@gmail.com>
2022-07-29 12:21:00 +0900

    웹 이미지 로더 Glide 활용

--- app/src/main/java/com/neppplus/storeorderpractice_20220729/adapters/StoreListViewAdapter.kt
+++ app/src/main/java/com/neppplus/storeorderpractice_20220729/adapters/StoreListViewAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.bumptech.glide.Glide
 import com.neppplus.storeorderpractice_20220729.datas.StoreData
 import kotlinx.android.synthetic.main.store_list_item.view.*
 
@@ -28,6 +29,9 @@ class StoreListViewAdapter (
 
         row.titleTxt.text = data.title
         row.scoreRatingBar.rating = data.score.toFloat()
+        Glide.with(mContext)           // with(context 자료형) => 어느 화면에서 사용할건지
+            .load(data.imgUrl)         // load(string으로 구성된 url주소) => 어떤 이미지 사용할건지
+            .into(row.storeImageView)  // into(target => 사용할 이미지뷰) => 어느 View에 넣을 건지
 
         return row
     }
