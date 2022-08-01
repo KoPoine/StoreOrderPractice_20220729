6aa90e5b81156b92b1b401d8957a04ba235937e3
넵플러스 <nepp.plus@gmail.com>
2022-08-01 10:11:11 +0900

    프로필 상세 보기 화면 생성

---
+++ app/src/main/java/com/neppplus/storeorderpractice_20220729/DetailProfileImageActivity.kt
@@ -0,0 +1,11 @@
+package com.neppplus.storeorderpractice_20220729
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailProfileImageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_profile_image)
+    }
+}
